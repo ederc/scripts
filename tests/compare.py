6b620e2fb543c38dc9754d45a931d738d5a04584
Christian Eder <ederc@mathematik.uni-kl.de>
2011-11-18 13:32:06 +0100

    adds a python file for comparing f5 and std in Singular for correctness of standard bases NOTE: For this the new .singularrc file in this folder is needed

---
+++ tests/compare.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+#############################################
+# this script uses two Singular programs
+# the respective std implementation against
+# each other.
+#############################################
+
+import re
+import glob
+import os
+
+# set paths for comparison
+pathsig   = "/home/ederc/uni/repos/devel/Singular"
+pathstd   = "/home/ederc/uni/repos/singular/Singular"
+pathcurr  = os.getcwd()
+# compute data
+os.system(pathsig+'/Singular -q > testsig.txt')
+os.system(pathstd+'/Singular -q > teststd.txt')
+
+# write all data to be compared into 
+# compare.txt
+# NOTE: do not forget to add ";" at the end of each
+#       line in order to achieve correct Singular
+#       code
+compare = open('compare.txt','w')
+
+# initialize first ideal
+testsig = open('testsig.txt','r')
+lines = testsig.readlines()
+
+# first of all:
+# define ring stuff out of first 3 lines
+compare.write('ring r = ('+\
+      lines[0].replace('\n','')+'),('+\
+      lines[1].replace('\n','')+'),('+\
+      lines[2].replace('\n','')+');\n')
+compare.write('ideal g;\n')
+for i in range(3,len(lines)):
+  lines[i] = lines[i].replace('\n',';\n')
+  compare.write(lines[i])
+testsig.close()
+  
+# initialize second ideal
+compare.write('ideal G;\n')
+teststd = open('teststd.txt','r')
+lines = teststd.readlines()
+for i in range(3,len(lines)):
+  lines[i] = lines[i].replace('\n',';\n')
+  lines[i] = lines[i].replace(lines[i][0],'G')
+  compare.write(lines[i])
+teststd.close()
+
+# reduce the standard basis against each other
+# store the result in a list l
+# if there exists at least one nonzero entry in l
+# then print 1,
+# else print 0
+compare.write('attrib(g,"isSB",1);')
+compare.write('attrib(G,"isSB",1);')
+compare.write('list l = reduce(g,G);\nint tt=0;\n')
+# NOTE: In Singular size(ideal i) gives you the number of
+#       nonzero generators in i. 
+# => checking if size(l[1])>0 is enough for us
+compare.write('if(size(l[1])!=0){tt=1;}\n')
+compare.write('printf("reduce STD with SIG: %s",tt);')
+compare.write('l = reduce(G,g);\n')
+compare.write('if(size(l[1])!=0){tt=1;}\n')
+compare.write('printf("reduce SIG with STD: %s",tt);')
+compare.write('$')
+compare.close()
+os.system(pathstd+'/Singular -q --no-rc < '+pathcurr+'/compare.txt')
